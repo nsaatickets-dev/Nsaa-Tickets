dd85165ef2a7a3cb4e2be2007912c4f6765a5901
Ernest Abel Nanabanyin Mensah <160252765+theernestmensah@users.noreply.github.com>
2026-07-13 15:58:56 +0000

    Update Clerk configuration to production mode

--- public/js/clerk-config.js
+++ public/js/clerk-config.js
@@ -1,7 +1,8 @@
 (function () {
   window.NSAA_CLERK_CONFIG = {
-    publishableKey: "pk_test_ZGl2aW5lLWtpdHRlbi00NS5jbGVyay5hY2NvdW50cy5kZXYk",
-    frontendApiUrl: "https://divine-kitten-45.clerk.accounts.dev",
+    publishableKey: "pk_live_Y2xlcmsubnNhYXRpY2tldHMuY29tJA",
+    frontendApiUrl: "https://clerk.nsaatickets.com",
     applicationName: "Nsaa Tickets",
   };
 })();
+
